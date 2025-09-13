204ab2d2dc4130ea1fa9c496fd2b273958f48045
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-09-13 19:00:26 +0530

    js added

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,1 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const params = new URLSearchParams(window.location.search);
-//   const name = params.get("name");
-//   const genre = params.get("genre");
-//   const img = params.get("img");
 
-//   if (name) document.getElementById("detailTitle").textContent = name;
-//   if (genre) document.getElementById("detailGenre").textContent = genre;
-//   if (img) document.getElementById("detailImage").src = img;
-// });
